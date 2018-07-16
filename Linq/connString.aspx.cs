26f1c0876c28af2fa1a55bc4d33a424942891335
Abiral Poudel <abiral.p95@gmail.com>
2018-07-16 13:19:51 +0545

    ADO

---
+++ Linq/connString.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Linq
+{
+    public partial class connString : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["abc"].ConnectionString;
+           using( SqlConnection con = new SqlConnection(CS)){
+                SqlCommand cmd = new SqlCommand("Select * from Students", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                
+
+
+            }
+
+        }
+    }
+}
